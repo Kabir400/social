caa36f74c2c4831d494498a963f52789390c9268
kabir <kabirehsan476@gamil.com>
2024-09-01 10:27:10 +0530

    solving the connection error

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const connectDB = require("./config/db.js");
-
 require("dotenv").config({ path: "./.env" });
+
+const connectDB = require("./config/db.js");
 
 const server = express();
 
